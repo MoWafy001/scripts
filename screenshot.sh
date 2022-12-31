19dea8a3e5fb3b14a81b1b6246f2bd369e957021
mowafy001 <wafy123445@gmail.com>
2022-12-31 07:01:24 +0200

    update the screenshot script to take arguments

--- screenshot.sh
+++ screenshot.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-scrot -s -f -F /tmp/screenshot.png && \
-  notify-send -t 1000 "screen shot taken" && \
+extraArgs=( "$@" )
+extraArgs="${extraArgs[@]:1}"
+msg=$1
+
+scrot $extraArgs -F /tmp/screenshot.png && \
+  notify-send -t 1000 "$msg" && \
   xclip -selection clipbloard -t image/png -i /tmp/screenshot.png && \
   rm /tmp/screenshot.png
