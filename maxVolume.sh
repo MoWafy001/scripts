9c0ba7e9a08a8822ff4d52a8013104ad58666f1a
mowafy001 <wafy123445@gmail.com>
2023-01-06 17:55:53 +0200

    change the step from 10 to 5

--- maxVolume.sh
+++ maxVolume.sh
@@ -7,8 +7,8 @@ if (($(echo -n $current_volume_pc | wc -m) == 0)); then
     current_volume_pc=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 fi
 
-if (($current_volume_pc < $max_volume_pc-10)) ; then
-    pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status
+if (($current_volume_pc < $max_volume_pc-5)) ; then
+    pactl set-sink-volume @DEFAULT_SINK@ +5% && $refresh_i3status
 else
     a=$(($max_volume_pc - $current_volume_pc))
     pactl set-sink-volume @DEFAULT_SINK@ +$a% && $refresh_i3status
