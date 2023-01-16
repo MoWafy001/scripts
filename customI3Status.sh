da692ab1b4c94684c1fb569509664bcca399f80b
mowafy001 <wafy123445@gmail.com>
2023-01-16 23:40:35 +0200

    create a module system for the custom i3staus warpper and a module to get the current warp latency

--- customI3Status.sh
+++ customI3Status.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# run i3status
 i3status | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && while :
 do
+  # load modules
+  modulesJSON=$(eval ~/scripts/i3StatusCustomModules/modules)
+
   read line
-  echo ",[\
-          {\"full_text\":\"test\", \"color\":\"#00FF00\" },\
-          ${line#,\[}" || exit 1
+  echo ",[$modulesJSON ${line#,\[}" || exit 1
 done)
