b6b6d99661fb6f76184b756df7a24398d85e5a36
mowafy001 <wafy123445@gmail.com>
2023-01-17 04:27:11 +0200

    remove logs

--- customI3Status.sh
+++ customI3Status.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-while :
+# run i3status
+i3status | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && while :
 do
-  echo $(date) started >> ~/Desktop/i3logs/i3status.log
-  # run i3status
-  i3status 2> ~/Desktop/i3logs/i3status-err.log | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && while :
-  do
-    read line
+  read line
 
-    # load modules
-    modulesJSON=$(eval ~/scripts/i3StatusCustomModules/modules)
-    echo $line >> ~/Desktop/i3logs/i3status.log 
-    echo $modulesJSON >> ~/Desktop/i3logs/i3status.log 
-    echo ",[$modulesJSON ${line#,\[}" || exit 1
-  done)
-  echo $(date) restarted >> ~/Desktop/i3logs/i3status.log 
-done
-
-notify-send "stopped"
+  # load modules
+  modulesJSON=$(eval ~/scripts/i3StatusCustomModules/modules)
+  echo ",[$modulesJSON ${line#,\[}" || exit 1
+done)
