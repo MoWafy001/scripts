1806aacff26451991df1fd0941568163cec2b691
mowafy001 <wafy123445@gmail.com>
2023-01-17 04:14:01 +0200

    set a timeout of 0.5 seconds for every module so the bar doesn't get stuck when a module takes too long

--- customI3Status.sh
+++ customI3Status.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-# run i3status
-i3status | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && while :
+while :
 do
-  # load modules
-  modulesJSON=$(eval ~/scripts/i3StatusCustomModules/modules)
+  echo $(date) started >> ~/Desktop/i3logs/i3status.log
+  # run i3status
+  i3status 2> ~/Desktop/i3logs/i3status-err.log | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && while :
+  do
+    read line
 
-  read line
-  echo ",[$modulesJSON ${line#,\[}" || exit 1
-done)
+    # load modules
+    modulesJSON=$(eval ~/scripts/i3StatusCustomModules/modules)
+    echo $line >> ~/Desktop/i3logs/i3status.log 
+    echo $modulesJSON >> ~/Desktop/i3logs/i3status.log 
+    echo ",[$modulesJSON ${line#,\[}" || exit 1
+  done)
+  echo $(date) restarted >> ~/Desktop/i3logs/i3status.log 
+done
+
+notify-send "stopped"
