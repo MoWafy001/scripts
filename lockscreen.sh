49d4b69262e469998ee50221d9f7f469f8a49fa1
mowafy001 <wafy123445@gmail.com>
2023-02-04 14:39:04 +0200

    ,ar

--- lockscreen.sh
+++ lockscreen.sh
@@ -59,7 +59,7 @@ lock_sc() {
   scrot -d 1 /tmp/locking_screen.png
   convert -blur 0x8 /tmp/locking_screen.png /tmp/screen_blur.png
   convert -composite /tmp/screen_blur.png ~/Pictures/lockBG.png -gravity South -geometry -20x1200 /tmp/screen.png
-  setxkbmap us
+  setxkbmap us,ar
   i3lock -i /tmp/screen.png
   revert
 }
